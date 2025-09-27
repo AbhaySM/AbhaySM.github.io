b5864900ef2582f1a1a146d99549435ef6555bee
Abhay Singh <abhay088@gmail.com>
2025-09-27 13:04:06 +0530

    Add dark mode + refine animations

--- appsmith/mascot/interactions.js
+++ appsmith/mascot/interactions.js
@@ -19,10 +19,9 @@ $(function () {
   $("nav a").click(function () {
     $(this).parent().find(".selected").removeClass("selected");
     $(this).addClass("selected");
-    var variant = $(this).attr("id");
-    $("#mascot-bounding-box").removeClass();
+    $("body").removeClass();
     $("nav .selected").each(function (i, e) {
-      $("#mascot-bounding-box").addClass($(e).attr("id"));
+      $("body").addClass($(e).attr("id"));
     });
   });
 });
