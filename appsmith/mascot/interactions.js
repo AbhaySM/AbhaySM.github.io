e9e9ee3c54a01c278b2a261b28f7a3ad766e0532
Abhay Singh <abhay088@gmail.com>
2025-09-27 10:52:37 +0530

    Attach sounds to connecting & speaking

--- appsmith/mascot/interactions.js
+++ appsmith/mascot/interactions.js
@@ -1,4 +1,21 @@
 $(function () {
+  //Sounds
+  var connecting = new Audio("sounds/connecting.wav");
+  var voice = new Audio("sounds/voice.wav");
+  connecting.loop = true;
+  $("#states a").click(function () {
+    connecting.pause();
+    voice.pause();
+  });
+  $("#connecting").click(function () {
+    connecting.currentTime = 0;
+    connecting.play();
+  });
+  $("#speaking").click(function () {
+    voice.currentTime = 0;
+    voice.play();
+  });
+
   $("nav a").click(function () {
     $(this).parent().find(".selected").removeClass("selected");
     $(this).addClass("selected");
